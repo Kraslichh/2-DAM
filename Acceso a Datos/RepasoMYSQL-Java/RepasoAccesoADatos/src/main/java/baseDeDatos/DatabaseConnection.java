804f6d113d8a2014fa627dfb4e12dc0c69725a98
Kraslichh <maxikirschberg1@icloud.com>
2023-10-24 13:31:58 +0200

    Ejercicio de clase tema 2
    Todos los ejercicios estan en el proyecto pero la base de datos no, pero si esta el conector

---
+++ Acceso a Datos/RepasoMYSQL-Java/RepasoAccesoADatos/src/main/java/baseDeDatos/DatabaseConnection.java
@@ -0,0 +1,31 @@
+package baseDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+	Connection connection;
+	
+	public Connection getConnection () {
+		String dbName = "acceso_datos";
+		String userName = "root";
+		String password = "";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost/" + dbName,
+					userName,
+					password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return connection;
+		
+	}
+}
---
+++ Acceso a Datos/RepasoMYSQL-Java/RepasoAccesoADatos/src/main/java/clases/ConectarBd.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class ConectarBd {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
