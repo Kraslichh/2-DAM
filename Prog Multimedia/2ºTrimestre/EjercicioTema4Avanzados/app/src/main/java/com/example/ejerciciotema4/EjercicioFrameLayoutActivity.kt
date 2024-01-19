4151a342b7a10caae77e355fdbf527590de8a4c9
Kraslichh <maxikirschberg1@icloud.com>
2024-01-20 00:11:08 +0100

    Tema4EjercicioAvanzado

---
+++ Prog Multimedia/2ºTrimestre/EjercicioTema4Avanzados/app/src/main/java/com/example/ejerciciotema4/EjercicioFrameLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ejerciciotema4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ejerciciotema4.R.layout.activity_main
+
+class EjercicioFrameLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+        // Tu código aquí
+    }
+}
